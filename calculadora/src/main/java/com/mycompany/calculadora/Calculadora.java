6703342c6ae56eafe2e400132999f047836b9c4e
Luis Javier <javiericm33@gmail.com>
2024-06-15 12:16:58 -0600

    Add files via upload

---
+++ calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.calculadora;
+
+import logica.HacerCalculadora;
+
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        HacerCalculadora calc = new HacerCalculadora();
+        calc.ventana();
+        
+    }
+}
---
+++ calculadora/src/main/java/logica/HacerCalculadora.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+
+import igu.Inicio;
+
+/**
+ *
+ * @author PILARES
+ */
+public class HacerCalculadora {
+
+    public HacerCalculadora() {
+    }
+    
+    public void ventana(){
+        
+        Inicio ventana1 = new Inicio();
+        ventana1.setVisible(true);
+        ventana1.setTitle("Inicio");
+        ventana1.setResizable(false);
+        ventana1.setLocationRelativeTo(null);
+        
+    }
+    
+}
